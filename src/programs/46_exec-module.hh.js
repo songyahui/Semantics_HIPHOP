f4431d6e9f67fff134e8814306e63933bd96c975
songyahui <songyahuiapp@hotmail.com>
2021-04-06 11:49:14 +0800

    m

--- src/programs/46_exec-module.hh.js
+++ src/programs/46_exec-module.hh.js
@@ -1,13 +1,19 @@
 "use hopscript"
 
-hiphop module M1( a ) {
+hiphop module M1( a ) 
+   /*@ requires emp    @*/
+   /*@ ensures  {a}.({}^*||{B}.{a}) @*/
+{
    emit a( 100 );
    async a {
-      this.notify( 10 );
+      emit B; this.notify( 10 );
    }
 }
 
-hiphop module m( a, b ) {
+hiphop module m( a, b ) 
+ /*@ requires emp    @*/
+/*@ ensures  ({a}.({}^*||{B}.{a})). ({a}.({}^*||{B}.{a})) @*/
+{
    run M1( a  );
    yield;
    run M1( a );
