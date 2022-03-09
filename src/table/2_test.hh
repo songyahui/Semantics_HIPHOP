1ff108b62cdb7dedac01f0686e5c3703a880faad
Yahui Song <songyahuiapp@hotmail.com>
2022-03-09 18:06:11 +0800

    m

---
+++ src/table/2_test.hh
@@ -0,0 +1,43 @@
+hiphop module prg(out Start, in A, in B, in C, in D, out O, out Done ) 
+   /*@ requires "True && emp "@*/
+// /*@ ensures  "True && ({})^*.{Done}" @*/
+// /*@ ensures  "True && ({}.{}).{Done}.({}^*)" @*/
+// /*@ ensures  "True && ({}.{A}).{C}.({}^*)" @*/
+// /*@ ensures  "True && ({}.{A})^*.{C}.({}^*)" @*/
+// /*@ ensures  "True && ({})^*.{A}.({}^*)" @*/
+
+// /*@ ensures  "True && ({}.{A})^*.{C}^*" @*/
+// /*@ ensures  "True && ({}.{}).A?" @*/
+// /*@ ensures  "True && ({}.{C}).{Start}" @*/
+// /*@ ensures  "True && ({}.{Done})^*.{C}" @*/
+ /*@ ensures  "True && ({}.{})^*.{Start}^*" @*/
+{
+   emit Start();
+   yield;
+   async Done {
+      emit A();
+      if (B.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()};  
+   };
+   emit B (); 
+   await Done;
+   loop{
+      emit Start();
+   yield;
+   async Done {
+      emit A();
+      if (B.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()};  
+   };
+   emit B (); 
+   await Done;
+   }
+}
