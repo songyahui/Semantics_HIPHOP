7a480c37e96ec9879569343021a64f9f5345486f
songyahui <songyahuiapp@hotmail.com>
2022-03-10 11:02:35 +0800

    m

---
+++ src/table/16_test.hh
@@ -0,0 +1,329 @@
+hiphop module prg(out Start, in A, in B, in C, in D, out O, out Done ) 
+   /*@ requires "True && emp "@*/
+/*@ ensures  "True && ({})^*.{Done}" @*/
+/*@ ensures  "True && (({})^*.{Done})^*" @*/
+/*@ ensures  "True && ({}.{A}).{C}.({})^*" @*/
+/*@ ensures  "True && ({Start}.({})^*)^*" @*/
+/*@ ensures  "True && ({})^*.{A}.({})^*" @*/
+/*@ ensures  "True && ({})^*.{Done}.{}^*" @*/
+/*@ ensures  "True && ({})^*.{Start}.{}^*" @*/
+/*@ ensures  "True && ({})^*.{}.{}^*" @*/
+/*@ ensures  "True && ({Start}.{A, B, Done}.({})^*)^*" @*/
+/*@ ensures  "True && (({Start}.{A, B, Done}.({})^*)^*) \// ({})^*.{}.{}^*" @*/
+/*@ ensures  "True && (({Start}.{A, B, Done}.({})^*)^*) \//  ({}.{}).B?" @*/
+
+
+/*@ ensures  "True && ({}.{}).A?" @*/
+/*@ ensures  "True && ({}.{}).B?" @*/
+/*@ ensures  "True && ({}.{})^*.A?" @*/
+/*@ ensures  "True && ({}^*.{C}.{D}.({}^*))^*" @*/
+/*@ ensures  "True && ({}.{C}).{Start}" @*/
+/*@ ensures  "True && ({}.{Done})^*.{C}.{Start}" @*/
+/*@ ensures  "True && ({}.{Done})^*.{C}.({Done}^*).{Start}" @*/
+/*@ ensures  "True && ({}.{})^*.{C}.{Start}" @*/
+/*@ ensures  "True && ({}.{}.{})^*.{C}" @*/
+/*@ ensures  "True && ({}.{})^*.{C}.{Start} \// ({}.{}.{})^*.{C}" @*/
+/*@ ensures  "True && ({}.{}).A? \//  ({}.{}).B?" @*/
+{
+   if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   };
+
+    if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   };
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+    yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   };
+
+   if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   };
+
+   if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+if (Done.now) {
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   } else {
+yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+   }
+   };
+
+        yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+
+      yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+
+         yield;
+   emit Start();
+   yield;
+      emit A();
+     
+         yield; 
+         emit C();
+    
+         yield; 
+         emit D();  
+   emit B (); 
+   emit Done ();
+
+   try {
+   loop{
+      emit Start();
+   yield;
+   {async Done {
+      emit A();
+      if (B.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()};  
+   };
+   emit B (); 
+   emit Done ();};
+   raise 0;
+   }}
+   catch {
+      emit C();
+   };
+
+    try {
+   loop{
+      emit Start();
+   yield;
+   {async Done {
+      emit A();
+      if (B.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()};  
+   };
+   emit B (); 
+   emit Done ();};
+   raise 0;
+   }}
+   catch {
+      emit C();
+   };
+
+   loop{
+      emit Start();
+   yield;
+   {async Done {
+      emit A();
+      if (B.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()};  
+      if (C.now) {
+         yield; 
+         emit C()}
+      else {
+         yield; 
+         emit D()}; 
+   };
+   emit B (); 
+   emit Done ();}
+   }
+}
+
+//30.889 237.2522 0.07164
